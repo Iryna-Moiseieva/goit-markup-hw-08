fa3c2322db6e2f93f93cdeb01798a03aed85e218
Iryna Moiseieva <arristotell8@gmail.com>
2023-11-01 18:25:37 +0100

    changed portfolio filter button style

--- js/filter.js
+++ js/filter.js
@@ -4,11 +4,14 @@
     portfolioItems: document.querySelectorAll('.portfolio__item'),
   };
 
-    refs.filterButtons.forEach(button => {
-    button.addEventListener('click', function () {
+    refs.filterButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+      refs.filterButtons.forEach((btn) => btn.classList.remove("portfolio__filter-button--active"));
+        button.classList.add("portfolio__filter-button--active");
+
       const filterText = button.textContent.trim();
 
-      refs.portfolioItems.forEach(item => {
+      refs.portfolioItems.forEach((item) => {
         const itemText = item.querySelector('.portfolio__text').textContent.trim();
 
         if (filterText === 'Всі' || filterText === itemText) {
